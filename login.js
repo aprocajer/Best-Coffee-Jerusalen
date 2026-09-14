c63d0fa6df6e11a5d79bfde23f791e1f6c90787c
aprocajer <aprocajer@gmail.com>
2026-09-14 16:32:31 -0600

    Update authentication script URL in login.js

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const mensajeLogin = document.getElementById("mensaje-login");
 
     // URL de tu script de autenticación (lo configuraremos en el backend)
-    const AUTH_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzjAjjE51EfuBbwn04USrkUSDp2PmU_3Y1ClAmEOP78o6ZolrnYOAGzCdMc1DF-37sB/exec";
+    const AUTH_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxcVvZajOTn8u2SbpLuN5y9-OaaBYNQfL4eEIR0VcGY-iVrsICueYBDL3kdUtohWMtx/exec";
 
     // Abrir modal
     if (btnLogin && modalLogin) {
